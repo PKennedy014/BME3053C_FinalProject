598b5f59e9bdd1e4342b0c645d50d406fbbe196f
PKennedy014 <97614064+PKennedy014@users.noreply.github.com>
2022-04-20 17:37:11 -0400

    Add files via upload

--- d_tree.m
+++ d_tree.m
@@ -8,14 +8,15 @@ model = fitctree(Tbl,'labels'); %Built-in to create decision tree model
 view(model,'mode','graph')
 
 %Create scatter of prediction at all points w overlay of actuals 
-% A = Tbl{:,2}; A=A';
-% B = Tbl{:,3}; B=B';
-% C = Tbl{:,1}; C=C';
-% [a,b] = meshgrid(linspace(min(A),max(A),500), linspace(min(B),max(B),100));
-% a=a(:); b=b(:);
-% prediction = model.predict([a b]);
-% gscatter(a,b,prediction,'rb','.',1,'off')
-% hold on
-% gscatter(A,B,'rb','v^',[],'off')
+A = Tbl{:,2};
+B = Tbl{:,3};
+C = Tbl{:,1};
+[a,b] = meshgrid(linspace(min(A),max(A),500), linspace(min(B),max(B),100));
+a=a(:); b=b(:);
+prediction = model.predict([a b]);
+gscatter(a,b,prediction,'rb','.',1,'off')
+hold on
+gscatter(A,B,C,'rb','v^',[],'off')
+title('Tree Model Predictions vs. Training Labels')
 
 end
